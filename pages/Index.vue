b1c3f1f7859ab2ff03633783494b2b2d1eb9a35a
nubby0208 <109526476+gruTonubby@users.noreply.github.com>
2022-12-27 15:33:01 +1000

    test

--- pages/Index.vue
+++ pages/Index.vue
@@ -43,52 +43,51 @@ export default {
     customPopupCookie: false
   }),
   mounted () {
-    // this.$ga.page({
-    //   page: '/',
-    //   title: 'Strona główna',
-    //   location: window.location.href
-    // })
-    console.log('begin')
+    this.$ga.page({
+      page: '/',
+      title: 'Strona główna',
+      location: window.location.href
+    })
     this.checkAuth()
-    // if (this.$route.query['payment-status'] === 'fail') {
-    //   this.$message({
-    //     message: 'Płatność nie została zrealizowana',
-    //     type: 'warning',
-    //     duration: 3000
-    //   })
-    //   this.$router.push(this.$route.path)
-    // }
-    // if (this.$route.query['payment-status'] === 'success') {
-    //   this.$message({
-    //     message: 'Płatność została zrealizowana.',
-    //     type: 'success',
-    //     duration: 3000
-    //   })
-    //   if (this.$store.state.user.isLogged) {
-    //     this.$router.push('/moje-ogloszenia')
-    //   } else {
-    //     this.$router.push(this.$route.path)
-    //   }
-    // }
-    // if (this.$route.query.token) {
-    //   this.verifyToken()
-    // }
-    // if (this.$route.query['test-msg'] === 'success') {
-    //   this.$message({
-    //     message: 'Test success msg.',
-    //     type: 'success',
-    //     duration: 30000
-    //   })
-    // }
-    // if (this.$route.query['test-msg'] === 'fail') {
-    //   this.$message({
-    //     message: 'Test fail msg.',
-    //     type: 'warning',
-    //     duration: 30000
-    //   })
-    // }
-    // this.toggleCookiesPopup()
-    // this.toggleCustomPopup()
+    if (this.$route.query['payment-status'] === 'fail') {
+      this.$message({
+        message: 'Płatność nie została zrealizowana',
+        type: 'warning',
+        duration: 3000
+      })
+      this.$router.push(this.$route.path)
+    }
+    if (this.$route.query['payment-status'] === 'success') {
+      this.$message({
+        message: 'Płatność została zrealizowana.',
+        type: 'success',
+        duration: 3000
+      })
+      if (this.$store.state.user.isLogged) {
+        this.$router.push('/moje-ogloszenia')
+      } else {
+        this.$router.push(this.$route.path)
+      }
+    }
+    if (this.$route.query.token) {
+      this.verifyToken()
+    }
+    if (this.$route.query['test-msg'] === 'success') {
+      this.$message({
+        message: 'Test success msg.',
+        type: 'success',
+        duration: 30000
+      })
+    }
+    if (this.$route.query['test-msg'] === 'fail') {
+      this.$message({
+        message: 'Test fail msg.',
+        type: 'warning',
+        duration: 30000
+      })
+    }
+    this.toggleCookiesPopup()
+    this.toggleCustomPopup()
   },
   methods: {
     closeCustomPopup () {
@@ -106,9 +105,7 @@ export default {
       }
     },
     async checkAuth () {
-      console.log('auth check api')
       const result = await check()
-      console.log('auth check api')
       if (this.$store.state.user.isLogged && result.data === false) {
         await this.$store.dispatch('user/logout')
       }
