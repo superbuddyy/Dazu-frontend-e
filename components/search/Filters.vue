887e38cfd5cc5ae605154cf6d1552b085e6ebf4a
nubby0208 <nubby0208@outlook.com>
2023-03-20 12:15:08 +1000

    refix

--- components/search/Filters.vue
+++ components/search/Filters.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="filters">
+    <treeselect v-model="search.category" :multiple="true" :options="filters.categories" placeholder="Wybierz" />
     <el-dialog
       title="Ustaw filtry"
       :visible.sync="visible"
       :before-close="close"
     >
-    <treeselect v-model="search.category" :multiple="true" :options="filters.categories" placeholder="Wybierz" />
+    
       <!-- <el-form v-if="Object.keys(filters).length > 0" :label-position="'top'" :model="filters" class="form">
         <div class="first-line">
           <el-form-item label="Kategoria" prop="category" class="category">
