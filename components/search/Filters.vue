f12f53be65d09374e4bf70f3dea4e57e8989900c
nubby0208 <nubby0208@outlook.com>
2023-03-28 00:44:12 +1000

    test

--- components/search/Filters.vue
+++ components/search/Filters.vue
@@ -13,9 +13,24 @@
               :options="filters.categories"
               placeholder="Wybierz"
             />
+      <el-form v-if="Object.keys(filters).length > 0" :label-position="'top'" :model="filters" class="form">
+        <div class="first-line">
+          <el-form-item label="Kategoria" prop="category" class="category">
+            <!-- <el-cascader
+              v-model="search.category"
+              :options="filters.categories"
+              :props="{ expandTrigger: 'hover', label: 'name', value: 'slug', children: 'children', checkStrictly: true }"
+              popper-class="category-dropdown"
+              clearable
+            /> -->
+            
+          </el-form-item>
+          <el-form-item label="Typ" prop="type">
             <el-select v-model="search.typ" clearable>
               <el-option v-for="type in filters.types" :key="type.slug" :label="type.name" :value="type.slug"/>
             </el-select>
+          </el-form-item>
+          <el-form-item label="Lokalizacja" prop="location">
             <el-select
               v-model="location"
               class="location-input"
@@ -35,8 +50,8 @@
                 :value="loc.lat + '*' + loc.lon + '*' + loc.display_name"
               />
             </el-select>
-      <el-form v-if="Object.keys(filters).length > 0" :label-position="'top'" :model="filters" class="form">
-        
+          </el-form-item>
+        </div>
         <div class="third-line-filters">
           <div class="attribute-filters">
             <div class="price">
