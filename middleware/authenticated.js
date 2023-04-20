b0cfdac0d333c2a4fda8b47eb5b308ba94c8992d
nubby0208 <nubby0208@outlook.com>
2023-04-20 13:18:33 +0900

    test

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,6 @@
 export default function (context) {
   console.log('store')
-  console.log(context.$store)
+  console.log(context.store)
   // If the user is not authenticated
   // if (!store.state.user.isLogged) {
   //   return redirect('/?next=' + route.path)
