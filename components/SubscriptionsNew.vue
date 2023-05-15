d1ba473c3ee131cfc6ddf5f515d2b017b3054285
nubby0208 <nubby0208@outlook.com>
2023-05-15 23:40:10 +0900

    test

--- components/SubscriptionsNew.vue
+++ components/SubscriptionsNew.vue
@@ -37,10 +37,10 @@
                     <span v-else-if='subscription.number_of_refreshes > 1 && subscription.number_of_refreshes < 5'>darmowe odświeżenia</span>
                     <span v-else-if='subscription.number_of_refreshes >= 5'>darmowych odświeżeń</span>
                   </div>
-                  <div class="list-item" v-if="subscription.featured_on_search_results_and_categories === true">
+                  <div class="list-item" v-if="subscription.name === 'Złote'">
                     <i class="el-icon-star-on" /> wyróżnione w ruchomej galerii w wynikach wyszukiwań i kategoriach
                   </div>
-                  <div class="list-item" v-if="subscription.featured_on_homepage === true">
+                  <div class="list-item" v-if="subscription.name === 'Złote' || subscription.name === 'Srebrne'">
                     <i class="el-icon-star-on" /> wyróżnione w ruchomej galerii na stronie, głównej w wynikach
                     wyszukiwań i kategoriach
                   </div>
